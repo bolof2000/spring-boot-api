ab0267cb96a1b6cc0b3862d7637706969f6c258a
Victor Bolofinde <bolofindeolusegun@yahoo.com>
2021-08-11 08:27:41 -0600

    some Integration tests

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.entity.ThemeParkRide;
+import com.example.demo.repository.ThemeParkRespository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +14,15 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner SampleData(ThemeParkRespository respository){
+
+		return (args -> {
+
+			respository.save(new ThemeParkRide("RollerCoaster","Train ride that speeds you along",5,3));
+			respository.save(new ThemeParkRide("Log Flume","Boat ride with plenty of splashes",3,2));
+			respository.save(new ThemeParkRide("Tearcups","Spinning ride in a giant tea-cup",2,4));
+
+		});
+	}
 }
