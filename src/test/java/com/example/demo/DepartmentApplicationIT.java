ebbf36bd3f32983f3677d87f9c3eab6979e6086b
Victor Bolofinde <bolofindeolusegun@yahoo.com>
2021-08-26 06:37:58 -0600

    test

---
+++ src/test/java/com/example/demo/DepartmentApplicationIT.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DepartmentApplicationIT {
+
+   @Autowired
+   private MockMvc mockMvc;
+
+
+   @Test
+   public void getAllDepartments() throws Exception {
+
+      mockMvc.perform(MockMvcRequestBuilders.get("/")
+      .accept(MediaType.APPLICATION_JSON))
+              .andExpect(status().isOk())
+              .andReturn();
+
+   }
+
+}
